c489ddb17108bdb0de496e4ce398415a811f7427
Devankshi Deshmukh <68735282+curry-dev@users.noreply.github.com>
2026-02-10 08:23:15 -0800

    fixed query structure

--- database/triggers.sql
+++ database/triggers.sql
@@ -1,17 +1,17 @@
+-- audit players
+
 CREATE TRIGGER telemetry.trg_players_audit
 ON core.players
 AFTER UPDATE
 AS
 BEGIN
     INSERT INTO telemetry.audit_log
-        (table_name, record_id, action_type, old_values, new_values, changed_by)
+        (table_name, operation, changed_at, changed_by)
     SELECT
         'players',
-        i.player_id,
         'UPDATE',
-        CONCAT('xp=', d.xp, ', coins=', d.coins),
-        CONCAT('xp=', i.xp, ', coins=', i.coins),
-        i.user_id
+        GETDATE(),
+        i.player_id
     FROM inserted i
     JOIN deleted d
         ON i.player_id = d.player_id;
